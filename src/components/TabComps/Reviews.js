dc3cad9e2f737983087c12ce1c84eb4af7580e9b
zenott <mblevel@gmail.com>
2018-10-19 11:43:31 +0200

    Finish Reviews

--- src/components/TabComps/Reviews.js
+++ src/components/TabComps/Reviews.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { hr } from '../Style/style.module.css';
 
 const Reviews = ({ data }) => {
   if (!data.reviews.results.length) {
     return <div>No reviews yet...</div>;
   }
   return data.reviews.results.map(x => (
-    <div key={x.id}>
-      <h5>{x.author}</h5>
-      <p>{x.content}</p>
-    </div>
+    <React.Fragment key={x.id}>
+      <div>
+        <h4 className="mt-4 mb-3">{`A review by ${x.author}`}</h4>
+        <p style={{ whiteSpace: 'pre-line' }}>{x.content}</p>
+      </div>
+      <hr className={hr} />
+    </React.Fragment>
   ));
 };
 
